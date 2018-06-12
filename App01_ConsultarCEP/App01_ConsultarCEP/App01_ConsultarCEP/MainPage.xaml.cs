6e8b4264b80960c150f166644a3cb1d033a35242
Igor Noronha <igoru.noronha@gmail.com>
2018-06-12 01:19:12 -0300

    App01 funcionando e retornando endereço.

--- App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
+++ App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 using Xamarin.Forms;
+using App01_ConsultarCEP.Servico.Modelo;
+using App01_ConsultarCEP.Servico;
 
 namespace App01_ConsultarCEP
 {
@@ -13,5 +12,55 @@ namespace App01_ConsultarCEP
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void BtnBuscar_Clicked(object sender, EventArgs e)
+        {
+
+            string cep = entCep.Text.Trim();
+
+            try
+            {
+                if (cep.Contains("-")) { cep = cep.Remove(cep.IndexOf('-')); }
+
+                bool isnumeric = true;
+                char[] datachars = cep.ToCharArray();
+
+                foreach (var datachar in datachars) {
+                    if (char.IsDigit(datachar)) {
+
+                        isnumeric = true;
+
+                    }
+                    else
+                    {
+
+                        isnumeric = false;
+
+                    }
+                    if (isnumeric == false)
+                    {
+                        throw new Exception("Digite apenas números!");
+                    }
+
+                }
+                
+                //if (!isnumeric) { throw new Exception("Digite apenas números!"); }
+                if (cep.Length < 8) { throw new Exception("CEP muito pequeno!"); }
+                if (cep.Length > 8) { throw new Exception("CEP muito grande!"); }
+                if (cep.Contains(",") || cep.Contains(".")) { throw new Exception("Existem caracteres inválidos!\nDigite apenas números."); }
+                          
+                                
+                Endereco end = ViaCepServico.BuscarEnderecoViaCep(cep);
+
+                txtResultado.Text = string.Format("Endereço: {0} - {1}\nCidade: {2} - {3}", end.Logradouro, end.Bairro, end.Localidade, end.Uf);
+
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert(ex.Message, "CEP inválido", "Ok");
+                return;
+            }
+            
+        }
+    }
 }
---
+++ App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCepServico.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+namespace App01_ConsultarCEP.Servico
+{
+    class ViaCepServico
+    {
+
+        private static readonly string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            string novoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+
+            string conteudo = wc.DownloadString(novoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+        }
+    }
+}
