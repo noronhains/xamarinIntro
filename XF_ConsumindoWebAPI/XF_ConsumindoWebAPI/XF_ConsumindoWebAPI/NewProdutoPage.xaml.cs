3d809e5aeb2bb4ed8f6ee71ed15f25efaa1b10d0
Igor Noronha <igoru.noronha@gmail.com>
2018-06-17 02:33:37 -0300

    Terminado projeto de consumo da API criada em ASP.NET. Página de novo cliente está com teste de layout posicionado ao lado.

---
+++ XF_ConsumindoWebAPI/XF_ConsumindoWebAPI/XF_ConsumindoWebAPI/NewProdutoPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using XF_ConsumindoWebAPI.Models;
+using XF_ConsumindoWebAPI.Service;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_ConsumindoWebAPI
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewProdutoPage : ContentPage
+	{
+
+        DataService dataService;
+ 
+        public NewProdutoPage ()
+		{
+			InitializeComponent ();
+            dataService = new DataService();
+        }
+
+        private async void BtnAdicionar_Clicked(object sender, EventArgs e)
+        {
+            if (Valida())
+            {
+                Produto novoProduto = new Produto
+                {
+                    Nome = txtNome.Text.Trim(),
+                    Categoria = txtCategoria.Text.Trim(),
+                    Preco = Convert.ToDouble(txtPreco.Text)
+                };
+                try
+                {
+                    await dataService.AddProdutoAsync(novoProduto);
+                    await Navigation.PopAsync();
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Erro", ex.Message, "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Erro", "Dados inválidos", "OK");
+            }
+        }
+        
+        private bool Valida()
+        {
+            if (string.IsNullOrEmpty(txtNome.Text) || string.IsNullOrEmpty(txtNome.Text) || string.IsNullOrEmpty(txtNome.Text))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
