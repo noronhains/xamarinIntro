d5815fd8e23f038dfafd6c77237c9d9e08aea4e9
Igor Noronha <igoru.noronha@gmail.com>
2018-06-17 03:03:35 -0300

    Reformulando o projeto.

---
+++ XF_ConsumindoWebAPI/XF_ConsumindoWebAPI/XF_ConsumindoWebAPI/MainPage.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XF_ConsumindoWebAPI.Service;
+using XF_ConsumindoWebAPI.Models;
+
+namespace XF_ConsumindoWebAPI
+{
+	public partial class MainPage : ContentPage
+	{
+
+        DataService dataService;
+        List<Produto> produtos;
+
+		public MainPage()
+		{
+			InitializeComponent();
+            dataService = new DataService();
+            LimpaProduto();
+            AtualizaDados();
+		}
+
+        async void AtualizaDados()
+        {
+            produtos = await dataService.GetProdutosAsync();
+            listaProdutos.ItemsSource = produtos.OrderBy(item => item.Nome).ToList();
+        }
+
+        private void ToolbarItem_Activated(object sender, EventArgs e)
+        {
+
+            if (((ToolbarItem)sender).Text == toolAdd.Text)
+            {
+                Navigation.PushAsync(new NewProdutoPage());
+            }
+            else if (((ToolbarItem)sender).Text == toolRefresh.Text) {
+                AtualizaDados();
+            }
+            else if (((ToolbarItem)sender).Text == toolClean.Text)
+            {
+                LimpaProduto();
+            }
+            else
+            {
+
+            }
+
+        }
+
+        private async void OnAtualizar(object sender, EventArgs e)
+        {
+           if (Valida())
+           {
+               try
+               {
+                    var mi = ((MenuItem)sender);
+                    Produto produtoAtualizar = (Produto)mi.CommandParameter;
+
+                    produtoAtualizar.Nome = txtNome.Text;
+                    produtoAtualizar.Categoria = txtCategoria.Text;
+                    produtoAtualizar.Preco = Convert.ToDouble(txtPreco.Text);
+
+                    await dataService.UpdateProdutoAsync(produtoAtualizar);
+
+                    LimpaProduto();
+                    AtualizaDados();
+               }
+               catch (Exception ex)
+               {
+                    await DisplayAlert("Erro", ex.Message, "OK");
+               }
+            }
+            else
+            {
+                await DisplayAlert("Erro", "Dados inválidos", "OK");
+            }
+        }
+
+        private async void OnDeletar(object sender, EventArgs e)
+        {
+            try
+            {
+                var mi = ((MenuItem)sender);
+                Produto produtoDeletar = (Produto)mi.CommandParameter;
+                await dataService.DeletaProdutoAsync(produtoDeletar);
+                LimpaProduto();
+                AtualizaDados();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        private void ListaProdutos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var produto = e.SelectedItem as Produto;
+            txtNome.Text = produto.Nome;
+            txtCategoria.Text = produto.Categoria;
+            txtPreco.Text = produto.Preco.ToString();
+        }
+
+        private void ContentPage_Focused(object sender, FocusEventArgs e)
+        {
+            AtualizaDados();
+        }
+     
+        private async void BtnAdicionar_Clicked(object sender, EventArgs e)
+        {
+            if (Valida())
+            {
+                Produto novoProduto = new Produto
+                {
+                    Nome = txtNome.Text.Trim(),
+                    Categoria = txtCategoria.Text.Trim(),
+                    Preco = Convert.ToDouble(txtPreco.Text)
+                };
+                try
+                {
+                    await dataService.AddProdutoAsync(novoProduto);
+                    await Navigation.PopAsync();
+                    LimpaProduto();
+                    AtualizaDados();
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Erro", ex.Message, "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Erro", "Dados inválidos", "OK");
+            }
+        }
+
+        private bool Valida()
+        {
+            if (string.IsNullOrEmpty(txtNome.Text) || string.IsNullOrEmpty(txtNome.Text) || string.IsNullOrEmpty(txtNome.Text))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void LimpaProduto()
+        {
+            txtCategoria.Text = "";
+            txtNome.Text = "";
+            txtPreco.Text = "";
+        }
+    }
+}
---
+++ XF_ConsumindoWebAPI/XF_ConsumindoWebAPI/XF_ConsumindoWebAPI/Service/DataService.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using XF_ConsumindoWebAPI.Models;
+
+namespace XF_ConsumindoWebAPI.Service
+{
+    class DataService
+    {
+        HttpClient client = new HttpClient();
+
+        public async Task<List<Produto>> GetProdutosAsync()
+        {
+            try
+            {
+                string url = "http://noronhasoft.somee.com/api/Produtos";
+                var response = await client.GetStringAsync(url);
+                var produtos = JsonConvert.DeserializeObject<List<Produto>>(response);
+                return produtos;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task AddProdutoAsync(Produto produto)
+        {
+            try
+            {
+                string url = "http://noronhasoft.somee.com/api/Produtos/{0}";
+
+                var uri = new Uri(string.Format(url, produto.Id));
+
+                var data = JsonConvert.SerializeObject(produto);
+                var content = new StringContent(data, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = null;
+
+                response = await client.PostAsync(uri, content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception("Erro ao incluir produto");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task UpdateProdutoAsync(Produto produto)
+        {
+
+            string url = "http://noronhasoft.somee.com/api/Produtos/{0}";
+            var uri = new Uri(string.Format(url, produto.Id));
+
+            var data = JsonConvert.SerializeObject(produto);
+            var content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = null;
+            response = await client.PutAsync(uri, content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Erro ao atualizar o produto");
+            }
+        }
+
+        public async Task DeletaProdutoAsync(Produto produto)
+        {
+            string url = "http://noronhasoft.somee.com/api/Produtos/{0}";
+            var uri = new Uri(string.Format(url, produto.Id));
+            await client.DeleteAsync(uri);
+        }
+    }
+}
