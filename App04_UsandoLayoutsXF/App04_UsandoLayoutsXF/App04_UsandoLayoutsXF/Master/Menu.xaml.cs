5b503ef1d3ed85c47e68efe94afa0b04f8c6848f
Igor Noronha <igoru.noronha@gmail.com>
2018-06-20 11:25:38 -0300

    Página de menu feita e perfil1 em desenvolvimento.

---
+++ App04_UsandoLayoutsXF/App04_UsandoLayoutsXF/App04_UsandoLayoutsXF/Master/Menu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App04_UsandoLayoutsXF.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(((Button)sender).Text == "Miguel de Icaza")
+            {
+                Navigation.PushAsync(new Pages.Perfil1());
+            }
+            else if (((Button)sender).Text == "Sobre a Xamarin")
+            {
+                Navigation.PushAsync(new Pages.Xamarin());
+            }           
+                
+        }
+    }
+}
