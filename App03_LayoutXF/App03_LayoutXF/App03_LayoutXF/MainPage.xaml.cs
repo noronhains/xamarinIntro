66c52a276fb32d31dcdf63da20d316ec6669a069
Igor Noronha <igoru.noronha@gmail.com>
2018-06-19 23:59:01 -0300

    Terminado exemplificação dos layouts

--- App03_LayoutXF/App03_LayoutXF/App03_LayoutXF/MainPage.xaml.cs
+++ App03_LayoutXF/App03_LayoutXF/App03_LayoutXF/MainPage.xaml.cs
@@ -13,5 +13,29 @@ namespace App03_LayoutXF
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (((Button)sender).Text == "Stack")
+            {
+                Navigation.PushAsync(new Layouts.StackLayout.StackPage());
+            }
+            else if (((Button)sender).Text == "Relative")
+            {
+                Navigation.PushAsync(new Layouts.Relative.RelativePage());
+            }
+            else if (((Button)sender).Text == "Scroll")
+            {
+                Navigation.PushAsync(new Layouts.Scroll.ScrollPage());
+            }
+            else if (((Button)sender).Text == "Absolute")
+            {
+                Navigation.PushAsync(new Layouts.Absolute.AbsolutePage());
+            }
+            else if (((Button)sender).Text == "Grid")
+            {
+                Navigation.PushAsync(new Layouts.Grid.GridPage());
+            }
+        }
+    }
 }
